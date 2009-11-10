103dfa0b219037c3cab013c28cdf247e2c98aa0c
Kyle Wiest <kyle.wiest@gmail.com>
2009-11-09 16:12:49 -0800

    RVideo and FLVTool2 gems required

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,10 @@ Rails::Initializer.run do |config|
   # Flash Session Key Middleware
   config.middleware.use "FlashSessionCookieMiddleware"
   
+  # RVideo + FLVTool2
+  congig.gem "rvideo"
+  config.gem "flvtool2"
+  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
