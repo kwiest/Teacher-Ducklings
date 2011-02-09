e16ad4b52315b6fd1ccce5a90c34c8978b300077
Kyle Wiest <kyle.wiest@gmail.com>
2011-02-09 12:10:41 -0800

    Admin links working

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -1,58 +1,52 @@
-class LinksController < ApplicationController
-  before_filter :admin_required
-  layout "admin"
+class Admin::LinksController < AdminController
+  before_filter :load_link, :except => [:index, :new, :create]
   
   def index
-    @links = Link.find(:all)
+    @links = Link.all
   end
   
-  # GET /links/1
   def show
-    @link = Link.find(params[:id])
   end
 
-  # GET /links/new
   def new
     @link = Link.new
   end
 
-  # GET /links/1/edit
   def edit
-    @link = Link.find(params[:id])
   end
 
-  # POST /links
   def create
     @link = Link.new(params[:link])
     
     if @link.save
       flash[:success] = 'Link was successfully created.'
-      redirect_to @link
+      redirect_to admin_links_path
     else
-      render :action => "new"
+      render :new
     end
   end
 
-  # PUT /links/1
   def update
-    @link = Link.find(params[:id])
-    params[:link][:category_ids] ||= []
-    
     if @link.update_attributes(params[:link])
       flash[:success] = 'Link was successfully updated.'
-      redirect_to links_path
+      redirect_to admin_links_path
     else
-      render :action => "edit"
+      render :edit
     end
   end
 
-  # DELETE /links/1
   def destroy
-    @link = Link.find(params[:id])
     @link.destroy
     
     flash[:notice] = 'Link was successfully deleted.'
-    redirect_to links_path
+    redirect_to admin_links_path
+  end
+  
+  
+  protected
+  
+  def load_link
+    @link = load_model(Link)
   end
   
 end
