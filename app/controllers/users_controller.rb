718297680ee965ef8c855248a5b755f8f677de54
Kyle Wiest <kyle.wiest@gmail.com>
2011-02-08 16:46:09 -0800

    Users working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :logged_in?
-  before_filter :authorized?, :only => [:show, :edit, :update]
-  before_filter :admin_required, :except => [:show, :edit, :update]
-  layout "admin"
-  
-  # GET /users
-  def index
-    @users = User.all
-  end
-  
-  
-  # GET /users/1
+  before_filter :load_user
+
   def show
-    @videos = @user.videos
-    render :layout => 'application' unless current_user.admin?
   end
   
-  
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-  
-  
-  # GET /users/1/edit
   def edit
-    render :layout => 'application' unless current_user.admin?
   end
 
-  
-  # POST /users
-  def create
-    @user = User.new(params[:user])
-    
-    if @user.save
-      flash[:success] = "New user created!"
-      redirect_to users_path
-    else
-      render :new
-    end
-  end
-  
-  
-  # PUT /users/1
   def update
     if @user.update_attributes(params[:user])
       flash[:success] = 'User was successfully updated.'
@@ -53,29 +17,9 @@ class UsersController < ApplicationController
   end
   
   
-  # DELETE /users/1
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    
-    flash[:notice] = "User deleted."
-    redirect_to users_path
-    
-    rescue ActiveRecord::RecordNotFound
-      flash[:error] = "We're sorry, but the user you're looking for cannot be found."
-      redirect_to root_path
+  protected
+  
+  def load_user
+    @user = current_user
   end
-  
-  
-  private
-  
-  def authorized?
-    @user = User.find(params[:id])
-    @user.changeable_by?(current_user) || access_denied
-    
-    rescue ActiveRecord::RecordNotFound
-      flash[:error] = "We're sorry, but the user you're looking for cannot be found."
-      redirect_to root_path
-  end
-
 end
