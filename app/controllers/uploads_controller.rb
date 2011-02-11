401e483f25ddf0ad9e2969a7e6e46a14fcb2240c
Kyle Wiest <kyle.wiest@gmail.com>
2011-02-10 17:40:23 -0800

    Uploads controller uses fast_video for params to set file fields

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,7 +2,7 @@ class UploadsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-    @video = current_user.videos.create(params[:video])
+    @video = current_user.videos.create(params[:fast_video])
     @video.title = "#{current_user.full_name} - #{Date.today.to_s(:long)}"
     @video.save
     @video.send_later(:encode)
