d73c85f004dc50b766d2638392160d6098ddc25e
Kyle Wiest <kyle.wiest@gmail.com>
2012-02-12 17:23:59 -0800

    Use new ActionMailer API to send email

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
   
   def deliver_password_reset_instructions!
     reset_perishable_token!
-    UserMailer.deliver_password_reset_instructions(self)
+    UserMailer.password_reset_instructions(self).deliver
   end
   
   def user
