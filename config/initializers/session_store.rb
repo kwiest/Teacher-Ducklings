c098376a590a1387731dc89ed4d3e4e903c7a440
Kyle Wiest <kyle.wiest@gmail.com>
2012-02-12 19:03:53 -0800

    session_store is a method, not variable

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-TeacherDucklings::Application.config.session_store = :cookie_store, key: '_Teacher-Ducklings_session'
+TeacherDucklings::Application.config.session_store :cookie_store, key: '_Teacher-Ducklings_session'
 
