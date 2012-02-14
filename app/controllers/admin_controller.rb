0ee049eb58d467021c12a5f2474d896169ba0a76
Kyle Wiest <kyle.wiest@gmail.com>
2012-02-14 10:25:19 -0800

    Use admin? helper function in before filter

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,13 +2,10 @@ class AdminController < ApplicationController
   before_filter :admin_required
   layout 'admin'
   
-  def index
-  end
-  
   protected
   
   def admin_required
-    access_denied unless logged_in? && current_user.admin == true
+    access_denied unless logged_in? && admin?
   end
 
 end
