a7fd8a7c6ba6cbaab9cf3d4575e1071a19f60ae4
Kyle Wiest <kyle.wiest@gmail.com>
2012-02-14 10:28:10 -0800

    Redirecto to admin_root_path after successful admin login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,11 +6,10 @@ class UserSessionsController < ApplicationController
   def create  
     @user_session = UserSession.new(params[:user_session])  
     if @user_session.save  
-      current_user.last_login_at = Time.now
-      current_user.save
+      current_user.update_attributes(last_login_at: Time.now)
       flash[:success] = "Hi #{current_user.first_name}, so good to see you again!"  
       if current_user.admin?
-        redirect_to admin_path
+        redirect_to admin_root_path
       else
         redirect_to root_path
       end
