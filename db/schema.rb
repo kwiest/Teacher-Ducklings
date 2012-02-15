e7b78af3e45480e7ea2a7b719cb295a0ad4996f7
Kyle Wiest <kyle.wiest@gmail.com>
2012-02-15 15:07:49 -0800

    Remove delayed_jobs database table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120207210120) do
+ActiveRecord::Schema.define(:version => 20120215230600) do
 
   create_table "categories", :force => true do |t|
     t.string "name"
@@ -28,98 +28,85 @@ ActiveRecord::Schema.define(:version => 20120207210120) do
   end
 
   create_table "comments", :force => true do |t|
-    t.integer  "post_id"
-    t.integer  "user_id"
-    t.string   "name"
-    t.string   "email"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "post_id"
+    t.integer   "user_id"
+    t.string    "name"
+    t.string    "email"
+    t.text      "body"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "links", :force => true do |t|
-    t.string   "name"
-    t.string   "url"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "document_file_name"
-    t.string   "document_content_type"
-    t.integer  "document_file_size"
+    t.string    "name"
+    t.string    "url"
+    t.text      "description"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "document_file_name"
+    t.string    "document_content_type"
+    t.integer   "document_file_size"
   end
 
   create_table "meetings", :force => true do |t|
-    t.integer  "video_id"
-    t.date     "date"
-    t.string   "time"
-    t.boolean  "confirmed"
-    t.integer  "creator_id"
-    t.integer  "user_to_meet_with_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "tok_session_id"
+    t.integer   "video_id"
+    t.date      "date"
+    t.string    "time"
+    t.boolean   "confirmed"
+    t.integer   "creator_id"
+    t.integer   "user_to_meet_with_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "tok_session_id"
   end
 
   create_table "posts", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "user_id"
+    t.string    "title"
+    t.text      "body"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "reviews", :force => true do |t|
-    t.integer  "video_id"
-    t.integer  "user_id"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "pdf_file_name"
-    t.string   "pdf_content_type"
-    t.integer  "pdf_file_size"
+    t.integer   "video_id"
+    t.integer   "user_id"
+    t.text      "description"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "pdf_file_name"
+    t.string    "pdf_content_type"
+    t.integer   "pdf_file_size"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",              :null => false
-    t.string   "crypted_password",   :null => false
-    t.string   "password_salt",      :null => false
-    t.string   "persistence_token",  :null => false
-    t.string   "perishable_token",   :null => false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "phone_number"
-    t.boolean  "admin"
-    t.datetime "last_login_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+    t.string    "email",              :null => false
+    t.string    "crypted_password",   :null => false
+    t.string    "password_salt",      :null => false
+    t.string    "persistence_token",  :null => false
+    t.string    "perishable_token",   :null => false
+    t.string    "first_name"
+    t.string    "last_name"
+    t.string    "phone_number"
+    t.boolean   "admin"
+    t.timestamp "last_login_at"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "photo_file_name"
+    t.string    "photo_content_type"
+    t.integer   "photo_file_size"
   end
 
   create_table "videos", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "description"
-    t.string   "video_file_name"
-    t.string   "state"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "zencoder_job_id"
-    t.text     "zencoder_error_message"
+    t.integer   "user_id"
+    t.string    "title"
+    t.text      "description"
+    t.string    "video_file_name"
+    t.string    "state"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "zencoder_job_id"
+    t.text      "zencoder_error_message"
   end
 
 end
