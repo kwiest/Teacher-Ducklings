8ddf2cbe29b7ee8d99e84e4c268750c36d61fb81
Kyle Wiest <kyle.wiest@gmail.com>
2012-02-16 14:52:02 -0800

    Add query to include posts and links

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,14 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :posts
   has_and_belongs_to_many :links
   
-  validates_presence_of :name
+  validates :name, presence: true
   
-  default_scope :order => :name
-  
+  default_scope order: :name
+
+  def self.find_with_posts_and_links(id)
+    where(id: id).includes(:posts, :links).first
+  end
+
   def to_param
     "#{id}-#{name.parameterize}"
   end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,4 @@
-require('spec_helper')
+require 'spec_helper'
 
 describe Category do
   it 'should validate' do
@@ -8,6 +8,15 @@ describe Category do
   it 'should have relationships' do
     should have_and_belong_to_many(:posts)
     should have_and_belong_to_many(:links)
+  end
+
+  it 'should be able to find a category by id and include its posts' do
+    category = Category.create!(name: "Math")
+    post = category.posts.create!(title: "Test post", body: "test post")
+    link = category.links.create!(name: 'Test link')
+
+    c = Category.find_with_posts_and_links(category.id)
+    c.should == category
   end
 
   it 'should have a more pretty url param' do
